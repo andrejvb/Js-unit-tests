c19116e4b0ebb304b32ff591e3e02595b32dfe13
andrejvb <andre.jvb@gmail.com>
2022-10-21 17:59:26 -0300

    finaliza requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -14,7 +14,9 @@
     estudante.name // Retorna: 'Leandrão, o Lobo Solitário'
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
-
-const createStudent = () => {};
+const createStudent = (nome) => {
+  let student = { name: nome, feedback: () => 'Eita pessoa boa!' };
+  return student;
+  };
 
 module.exports = createStudent;
